948cfbf781bb4d8702076bc10653d12f72f0bcd7
Ayan Sheikh <ayansheikh52@gmail.com>
2024-08-19 15:28:51 +0530

    changed implementation of hash table

--- handler.go
+++ handler.go
@@ -7,29 +7,32 @@ import (
 )
 
 var Handlers = map[string]func([]Value) Value{
-	"PING":    ping,
-	"SET":     set,
-	"GET":     get,
-	"HSET":    hsetHT,
-	"HGET":    hgetHT,
-	"HGETALL": hgetallHT,
-	"HDEL":    hdelHT,
-	"INCR":    incr,
-	"DECR":    decr,
-	"INCRBY":  incrBy,
-	"DECRBY":  decrBy,
-	"DEL":     del,
-	"APPEND":  appendto,
-	"LPUSH":   Lpush,
-	"LRANGE":  Lrange,
-	"LPOP":    Lpop,
-	"RPOP":    Rpop,
-	"RPUSH":   Rpush,
+	"PING":     ping,
+	"SET":      set,
+	"GET":      get,
+	"CHSET":    hsetHT,
+	"CHGET":    hgetHT,
+	"CHGETALL": hgetallHT,
+	"CHDEL":     hdelHT,
+	"INCR":     incr,
+	"DECR":     decr,
+	"INCRBY":   incrBy,
+	"DECRBY":   decrBy,
+	"DEL":      del,
+	"APPEND":   appendto,
+	"LPUSH":    Lpush,
+	"LRANGE":   Lrange,
+	"LPOP":     Lpop,
+	"RPOP":     Rpop,
+	"RPUSH":    Rpush,
+	"HSET":     hset,
+	"HGET":     hget,
+	"HGETALL":  hgetall,
 }
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
-		return Value{typ: "string", str: "PONG Mine"}
+		return Value{typ: "string", str: "PONG"}
 	}
 
 	return Value{typ: "string", str: args[0].bulk}
